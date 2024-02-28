cbf0830cdbb9feef95c50b21b6ae0e33f5d073ac
Kelsey Evans <thekelso91@gmail.com>
2024-02-28 02:02:00 -0600

    small methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -21,30 +21,27 @@ class GildedRose
   end
 
   def normal_item_tick
-   @days_remaining -= 1
-   return if @quality == 0
-
-   @quality -= 1
-   @quality -= 1 if @days_remaining <= 0
+   @item = Normal.new(@quality, @days_remaining)
+    @item.tick
   end
+
   def brie_tick
     @days_remaining -= 1
     return if @quality >= 50
-
     @quality += 1
     @quality += 1 if @days_remaining <= 0 && @quality < 50
   end
+
   def sulfuras_tick
 
   end
+
   def backstage_tick
     @days_remaining -= 1
     return if @quality >= 50
     return @quality = 0 if @days_remaining <= 0
-
     @quality += 1
     @quality += 1 if @days_remaining <= 10
     @quality += 1 if @days_remaining <= 5
-
   end
 end
