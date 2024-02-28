2134f9c2922922dc18828d754f7ac143e4d6ceb6
Kelsey Evans <thekelso91@gmail.com>
2024-02-28 03:11:55 -0600

    Made some fixes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,21 +1,20 @@
 module GildedRose
   class Item
-    attr_reader :name, :quality, :days_remaining
-
-    def initialize(name, quality, days_remaining)
-      @name, @quality, @days_remaining = name, quality, days_remaining
+    attr_reader :quality, :days_remaining
+    def initialize(quality:, days_remaining:)
+      @days_remaining = days_remaining
+      @quality = quality
     end
-
-    def tick
+    def tick 
     end
-  end
+  end 
 
-  class Normal < Item
+  class Normal Item < Item
     def tick
       @days_remaining -= 1
-      return if @quality == 0
+      return if @quality == 0 
 
-      @quality -= 1
+      @quality -= 1 
       @quality -= 1 if @days_remaining <= 0
     end
   end
@@ -23,32 +22,32 @@ module GildedRose
   class Brie < Item
     def tick
       @days_remaining -= 1
-      return if @quality >= 50
+      return if @quality >= 50 
 
-      @quality += 1
-      @quality += 1 if @days_remaining <= 0
+      @quality += 1 
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
     end
   end
+
 
   class Backstage < Item
     def tick
       @days_remaining -= 1
       return              if @quality >= 50
-      return @quality = 0 if @days_remaining <= 0
+      return @quality = 0 if @days_remaining < 0 
 
-      @quality += 1
-      @quality += 1 if @days_remaining < 10
+      @quality += 1 
+      @quality += 1 if @days_remaining < 10 
       @quality += 1 if @days_remaining < 5
     end
   end
 
   DEFAULT_CLASS = Item
-  SPECIALIZED_CLASSES = {
-    'normal' => Normal,
-    'Aged Brie' => Brie,
-    'Backstage passes to a TAFKAL80ETC concert' => Backstage }
-  
-  def self.new(name, quality, days_remaining)
+  SPECIALIZED_CLASSES = { 
+    'normal'                                        => Normal Item,
+    'Aged Brie'                                     => Brie,
+    'Backstage passes to a TAFKAL80ETC concert'     => Backstage}
+  def self.new(name:,quality:,days_remaining:)
     (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
   end
 end
